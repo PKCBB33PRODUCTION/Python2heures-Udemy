5740843d95425913f94cbc782bf0b8f4e11f1e9e
Noah PK33PROD <49820105+PKCBB33PRODUCTION@users.noreply.github.com>
2025-11-27 15:02:09 +0100

    Add L100-6.py for basic arithmetic operations

---
+++ L100/L100-6.py
@@ -0,0 +1,22 @@
+#=====================#
+#= PKCBB33PRODUCTION =#
+#=====================#
+
+nb1 = 1
+nb2 = -2
+nb3 = 3.5
+nb4 = nb2 * nb3
+
+print("nb1 = ", nb1)
+print("nb2 = ", nb2)
+print("nb3 = ", nb3)
+
+print("========================")
+print("nb2 * 2 =", nb2*2)
+print("========================")
+print("nb2 * nb3 =", nb2*nb3)
+print("nb2 / nb3 =", nb2/nb3)
+print("========================")
+print("nb2 + nb3 =", nb2+nb3)
+print("nb2 - nb3 =", nb2-nb3)
+print("========================")
