b5c1b1540a7595aed328b616965da553089404ce
PK33PROD <noah.maillet33pro@gmail.com>
2025-11-27 16:48:58 +0100

    Ajout - L200-18.py

---
+++ L200/L200-18.py
@@ -0,0 +1,24 @@
+# Auteur - Noah M.
+# 18. Découpez vos Chaînes : La Puissance de Split
+
+# Ecriture de base split.
+#string.split(separator,maxsplit)
+
+# Cas 1:
+stg = "My name is Noah"
+print(f"String = {stg}")
+
+# Decoupage de string.
+print(f"Decoupage stg via les espace : {stg.split()}")
+
+# Cas 2:
+stg2 = "Noah, Camille, Laurent, MarieF"
+print(f"String2 = {stg2}")
+
+# Decoupage de string2 - séparateur 'virgule'.
+print(stg2.split(","))
+
+# Cas 3:
+stg3 = "My name is Camille"
+print(f"String = {stg}")
+print(f"Decoupage max 2 & sep espace =>{stg3.split(" ",2)}")
