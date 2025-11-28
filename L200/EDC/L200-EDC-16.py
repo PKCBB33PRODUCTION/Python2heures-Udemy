9e75731965c4fd17d7919d06486e9546261cbde1
PK33PROD <noah.maillet33pro@gmail.com>
2025-11-28 11:35:17 +0100

    Ajout - EDC-16

---
+++ L200/EDC/L200-EDC-16.py
@@ -0,0 +1,9 @@
+# Auteur : Noah M.
+# Découper une Chaîne en Mots
+#Split directe :
+phrase = "Python est simple et puissant"
+print(phrase.split())
+
+#Split indirect :
+# result = phrase.split()
+# print(result)
