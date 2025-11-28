3d12a5e1b4775a8ecceca33b9631dc06b2d587b3
PK33PROD <noah.maillet33pro@gmail.com>
2025-11-28 16:42:54 +0100

    Ajout - L200-EDC-20.py

---
+++ L200/EDC/L200-EDC-20.py
@@ -0,0 +1,9 @@
+# Auteur : Noah MAILLET
+# Créer une Liste à partir d’un Dictionnaire
+d = {"a": 1, "b": 2, "c": 3}
+
+# Affichage des différents outil de
+list_d = d.items()
+result = list(list_d)
+
+print(result)
