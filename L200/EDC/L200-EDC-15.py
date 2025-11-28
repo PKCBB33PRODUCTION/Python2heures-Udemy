345e6e9428814a26c8317527546804d5ab5adf1a
PK33PROD <noah.maillet33pro@gmail.com>
2025-11-28 11:34:47 +0100

    Ajout - Commentaire - EDC 15

--- L200/EDC/L200-EDC-15.py
+++ L200/EDC/L200-EDC-15.py
@@ -1,7 +1,12 @@
 # Auteur : Noah M.
 # Créer et Parcourir un Dictionnaire
+# Création du dictionnaire.
 ages = {"Alice": 30, "Bob": 25}
+
+# Création d'une liste.
 result = []
+
+# Fonction qui parcours le dictionnaires.
 for nom, age in ages.items():
     result.append(f"{nom} a {age} ans.")
 print("\n".join(result))
